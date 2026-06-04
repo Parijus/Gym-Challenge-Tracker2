c424b09d26edbc5ced7098d78a3089a8ffb486be
gervansealy <134415588+gervansealy@users.noreply.github.com>
2026-06-04 17:37:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,1 @@
-// Replace after deploying Google Apps Script. Example: https://script.google.com/macros/s/XXXX/exec
-const API_URL = "https://script.google.com/macros/s/AKfycbw-B1zIZPXB4-T3robvGH19FhBMXKR-nljNXOzdblE8dLyn10Q8qnOaoLIhtGlwvGIZGA/exec";
-const LOCAL_DEMO_MODE = API_URL.includes("PASTE_YOUR");
-const MEMBERS = [
-  { name: "JC", pin: "1111", admin: false },
-  { name: "Niala", pin: "2222", admin: false },
-  { name: "Justin", pin: "3333", admin: true },
-  { name: "Gervan", pin: "4444", admin: false },
-  { name: "Ellie", pin: "5555", admin: false },
-  { name: "Gabe", pin: "6666", admin: false },
-  { name: "Jerell", pin: "7777", admin: false },
-  { name: "Nadine", pin: "8888", admin: false }
-];
+
